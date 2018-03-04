5dcd9fe2fba064f5b693c32918b1c0f1a9e96865
zachrule <36868124+zachrule@users.noreply.github.com>
2018-03-03 19:30:31 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<html>
+<link rel="stylesheet" href="style.css">
+<div class="footer">
+<footer>
+This site is part of a CSU <a href="https://www.cs.colostate.edu/~ct310/yr2018sp/index.php">CT 310</a> Course Project.
+</footer>
+</div>
+</html>
---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<div class="header">
+<img src="flag.jpg" alt="flag">
+<h4>North Dakota</h4>
+<div>
+</head>
+</html>
